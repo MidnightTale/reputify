a5316a614177052425452d1b753662afd1531750
MidnightTale <midnighttale24@gmail.com>
2024-03-09 17:01:01 +0700

    no prefix

--- src/main/java/net/hynse/reputify/ReputationManager.java
+++ src/main/java/net/hynse/reputify/ReputationManager.java
@@ -19,43 +19,4 @@ public class ReputationManager {
     public void updatePlayerReputation(UUID playerId, int newPoints) {
         mongoDBManager.updatePlayerReputation(playerId, newPoints);
     }
-
-    public String getPlayerPrefix(UUID playerId) {
-        Document playerReputation = getPlayerReputation(playerId);
-
-        if (playerReputation.containsKey("positive_reputation") && playerReputation.containsKey("negative_reputation")) {
-            int positiveReputationLevel = calculateReputationLevel(playerReputation.getInteger("positive_reputation"));
-            int negativeReputationLevel = calculateReputationLevel(playerReputation.getInteger("negative_reputation"));
-
-            switch (positiveReputationLevel) {
-                case 1:
-                    return "Newcomer";
-                case 2:
-                    return "Respected";
-                case 3:
-                    return "Veteran";
-            }
-
-            switch (negativeReputationLevel) {
-                case 1:
-                    return "Troublemaker";
-                case 2:
-                    return "Outcast";
-                case 3:
-                    return "Pariah";
-            }
-        }
-
-        return "Player";
-    }
-    public static int calculateReputationLevel(int reputationPoints) {
-        if (reputationPoints >= 7) {
-            return 3; // Highest reputation level
-        } else if (reputationPoints >= 3) {
-            return 2; // Medium reputation level
-        } else if (reputationPoints >= 1) {
-            return 1; // Lowest reputation level
-        }
-        return reputationPoints;
-    }
 }
